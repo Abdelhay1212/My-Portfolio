36aca6d51f221bf74d0a262e7fec6246e63b7447
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2024-05-22 00:49:59 +0100

    Finished the Hero section

---
+++ scripts/runFirst.js
@@ -0,0 +1,4 @@
+// Check for dark mode preference
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  document.body.classList.add('dark-mode');
+}
--- scripts/script.js
+++ scripts/script.js
@@ -7,10 +7,8 @@ burgerMenu.addEventListener('click', () => {
   navMenuLinks.classList.toggle('active');
 });
 
-
 // toggle the theme button
 const themeToggle = document.getElementById('theme-toggle');
-const nav = document.getElementById('nav');
 const moon = document.getElementById('moon');
 const sun = document.getElementById('sun');
 const body = document.body;
@@ -20,5 +18,43 @@ themeToggle.addEventListener('click', () => {
   sun.classList.toggle('hide');
 
   body.classList.toggle('dark-mode');
-  nav.classList.toggle('dark-mode');
 });
+
+
+// redirect the user to my github account and my resume
+const githubBtn = document.getElementById('githubBtn');
+const resumeBtn = document.getElementById('resumeBtn');
+// github link
+githubBtn.addEventListener('click', () => {
+  window.open('https://github.com/Abdelhay1212', '_blank');
+});
+// resume link
+resumeBtn.addEventListener('click', () => {
+  window.open('https://drive.google.com/file/d/1qyfr-jsf_BpTlDvT_gk-NqvrMAR9bWPS/view?usp=sharing', '_blank');
+});
+
+
+// animate the span inside the h1 tag
+const span = document.getElementById('animate-text');
+const new_text = 'a Backend Engineer';
+const old_text = span.innerText;
+
+setInterval(() => {
+  if (span.innerText == old_text) {
+    span.classList.remove('animate__fadeIn');
+    span.classList.add('animate__fadeOut');
+    setTimeout(() => {
+      span.innerText = new_text;
+      span.classList.remove('animate__fadeOut');
+      span.classList.add('animate__fadeIn');
+    }, 500);
+  } else {
+    span.classList.remove('animate__fadeIn');
+    span.classList.add('animate__fadeOut');
+    setTimeout(() => {
+      span.innerText = old_text;
+      span.classList.remove('animate__fadeOut');
+      span.classList.add('animate__fadeIn');
+    }, 500);
+  }
+}, 2000);
